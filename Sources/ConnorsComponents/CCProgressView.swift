89b8b2f9663511e1f53b77d1fc45caf07cd7d2b9
Connor Black <connor.b645@gmail.com>
2022-06-27 15:24:51 +0100

    Added CCProgressView

---
+++ Sources/ConnorsComponents/CCProgressView.swift
@@ -0,0 +1,44 @@
+//
+//  CCProgressView.swift
+//  
+//
+//  Created by Connor Black on 27/06/2022.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+public struct CCProgressView: View {
+    
+    let size: Double
+    let cornerRadius: Double
+    let foregroundColor: Color
+    let backgroundColor: Color
+        
+    public init(size: Double = 50.0,
+                cornerRadius: Double = 5.0,
+                foregroundColor: Color = Color.white,
+                backgroundColor: Color = Color.primary) {
+        self.size = size
+        self.cornerRadius = cornerRadius
+        self.foregroundColor = foregroundColor
+        self.backgroundColor = backgroundColor
+    }
+    
+    public var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: cornerRadius)
+                .fill(backgroundColor)
+                .frame(width: size, height: size)
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: foregroundColor))
+        }
+    }
+}
+
+@available(iOS 14.0, *)
+struct CCProgressView_Previews: PreviewProvider {
+    static var previews: some View {
+        CCProgressView()
+    }
+}
