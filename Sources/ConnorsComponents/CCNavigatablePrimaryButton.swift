f65ab03f9a35e991243a28c7876a3db70aab4029
Connor Black <connor.b645@gmail.com>
2022-06-28 11:37:30 +0100

    Added a navigatable primary button

--- Sources/ConnorsComponents/CCNavigatablePrimaryButton.swift
+++ Sources/ConnorsComponents/CCNavigatablePrimaryButton.swift
@@ -1,38 +1,38 @@
 //
-//  CCPrimaryButton.swift
+//  SwiftUIView.swift
 //  
 //
-//  Created by Connor Black on 27/06/2022.
+//  Created by Connor Black on 28/06/2022.
 //
 
 import SwiftUI
 
 @available(iOS 14.0, *)
-public struct CCPrimaryButton: View {
+public struct CCNavigatablePrimaryButton<Destination: View>: View {
+    let destination: Destination
     let title: String
     let textColor: Color
     let backgroundColor: Color
     let cornerRadius: Double
     let height: Double
-    let onTap: (() -> Void)?
     
     public init(title: String,
                 textColor: Color = .white,
                 backgroundColor: Color = .pink,
                 cornerRadius: Double = 5.0,
                 height: Double = 50,
-                onTap: (() -> Void)?) {
+                destination: () -> Destination) {
+        self.destination = destination()
         self.title = title
         self.textColor = textColor
         self.backgroundColor = backgroundColor
         self.cornerRadius = cornerRadius
         self.height = height
-        self.onTap = onTap
     }
     
     public var body: some View {
-        Button {
-            onTap?()
+        NavigationLink {
+            destination
         } label: {
             ZStack {
                 RoundedRectangle(cornerRadius: cornerRadius)
@@ -41,17 +41,17 @@ public struct CCPrimaryButton: View {
                 Text(title)
                     .foregroundColor(textColor)
             }
+            .frame(maxWidth: .infinity)
+            .frame(height: height)
         }
-        .frame(maxWidth: .infinity)
-        .frame(height: height)
     }
 }
 
 @available(iOS 14.0, *)
-struct CCPrimaryButton_Previews: PreviewProvider {
+struct CCNavigatablePrimaryButton_Previews: PreviewProvider {
     static var previews: some View {
-        CCPrimaryButton(title: "Button Title") {
-            print("Button Tapped")
+        CCNavigatablePrimaryButton(title: "Go to hello world") {
+            Text("Hello World")
         }
     }
 }
